9b868128fbfbe9afd411b2a47699393f35b86ea8
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2020-11-28 16:25:39 +0100

    basic wpf added

---
+++ TicketSwap/TicketSwap.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TicketApiClient;
+using TicketSwap.Shared;
+
+namespace TicketSwap.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        RestService<Ticket, string> _restService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _restService = new RestService<Ticket, string>("http://localhost:65383", "/api/ticket");
+        }
+
+        private void Send_Click(object sender, RoutedEventArgs e)
+        {
+            Ticket ticket = new Ticket();
+            ticket.EventName = textBox_eventName.Text;
+            ticket.EventDate = datePicker_eventDate.SelectedDate ?? DateTime.Now;
+            ticket.EventPrice = int.Parse(textBox_eventPrice.Text);
+            _restService.Post(ticket);
+        }
+
+        private async void Sync_Click(object sender, RoutedEventArgs e)
+        {
+            listBox.Items.Clear();
+            var tickets = await _restService.Get();
+            foreach (var item in tickets)
+            {
+                listBox.Items.Add(item);
+            }
+        }
+    }
+}
