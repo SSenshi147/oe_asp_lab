5f9b1330fedb3ac7ad3c6e8170ccc3c18eac3972
SSenshi147 <47696835+SSenshi147@users.noreply.github.com>
2020-11-30 00:53:06 +0100

    blazor added

--- TicketSwap/TicketSwap.Api/Startup.cs
+++ TicketSwap/TicketSwap.Api/Startup.cs
@@ -72,11 +72,13 @@ namespace TicketSwap.Api
                 builder.AllowCredentials();
             }));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "TicketSwap.Api", Version = "v1" });
-            });
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "TicketSwap.Api", Version = "v1" });
+            //});
             services.AddSignalR();
+            services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -85,11 +87,16 @@ namespace TicketSwap.Api
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketSwap.Api v1"));
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TicketSwap.Api v1"));
+                app.UseWebAssemblyDebugging();
             }
 
             app.UseHttpsRedirection();
+
+            app.UseBlazorFrameworkFiles();
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -101,8 +108,10 @@ namespace TicketSwap.Api
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllers();
                 endpoints.MapHub<TicketHub>("/api/ticketHub");
+                endpoints.MapFallbackToFile("index.html");
             });
         }
     }
